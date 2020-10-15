b6482140e2c952ad9d3f93e3a0a80defa16e13cc
Alejandro Triana <alejandro183@gmail.com>
2020-10-15 17:44:16 -0500

    Update ApiRestExample.sh

--- ApiRestExample.sh
+++ ApiRestExample.sh
@@ -8,12 +8,3 @@ git clone https://github.com/alejandro183/pythonApi.git
 
 echo "Downloading the mongo Image for Docker"
 docker pull mongo
-
-echo "Setting up the API rest env"
-pip3 install pipenv
-
-pipenv shell
-
-pip install flask
-pip install pymongo
-
