62a53b96be0f3bbfd5a62dbbfcf3bcc7af134d00
Alejandro Triana <alejandro183@gmail.com>
2020-10-15 19:27:56 -0500

    Update ApiRestExample.sh

--- ApiRestExample.sh
+++ ApiRestExample.sh
@@ -4,6 +4,9 @@ sudo yum upgrade -y
 sudo yum install python3 git -y
 sudo amazon-linux-extras install docker
 
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 echo "Downloading the project"
 git clone https://github.com/alejandro183/pythonApi.git
 
