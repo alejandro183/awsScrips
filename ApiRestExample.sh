42a604cb37169bd7e0af3c77323a29809219c660
Alejandro Triana <alejandro183@gmail.com>
2020-10-15 19:33:26 -0500

    Update ApiRestExample.sh

--- ApiRestExample.sh
+++ ApiRestExample.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 echo "Installing Utils and Environment"
 sudo yum upgrade -y
-sudo yum install python3 git -y
-sudo amazon-linux-extras install docker
+sudo yum install docker python3 git -y
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
